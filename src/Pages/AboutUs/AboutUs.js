1cc1bd178c7059794dbeea9f3ed513974dd11215
Md Rafi Uz Zaman <rafi.kazla@gmail.com>
2023-01-27 15:17:07 +0600

    final2

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -3,27 +3,27 @@ import React from 'react'
 const AboutUs = () => {
   return (
     <>
-     <div className="hero">
+     <div className="hero m-3">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="https://placeimg.com/260/400/arch" className="max-w-sm mask mask-diamond" />
+    <img src="https://images.unsplash.com/photo-1628348070889-cb656235b4eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="max-w-sm mask mask-diamond" />
     <div>
       <h1 className="text-5xl font-bold">Box Office News!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
   </div>
 </div>
-<div className="hero">
+<div className="hero m-3">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="https://placeimg.com/260/400/arch" className="max-w-sm mask mask-diamond" />
+    <img src="https://images.unsplash.com/photo-1628348070889-cb656235b4eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="max-w-sm mask mask-diamond" />
     <div>
       <h1 className="text-5xl font-bold">Box Office News!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
   </div>
 </div>
-<div className="hero">
+<div className="hero m-3">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="https://placeimg.com/260/400/arch" className="max-w-sm mask mask-diamond" />
+    <img src="https://images.unsplash.com/photo-1628348070889-cb656235b4eb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className="max-w-sm mask mask-diamond" />
     <div>
       <h1 className="text-5xl font-bold">Box Office News!</h1>
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
